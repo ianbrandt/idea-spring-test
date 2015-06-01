37197c781828e55e12182fb11adf66f59e7f0f7d
Ian Brandt <ian@ianbrandt.com>
2015-06-01 08:12:09 -0700

    Initial commit.

---
+++ src/test/java/DbTestConfig.java
@@ -0,0 +1,16 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DbTestConfig {
+
+	@Bean
+	public DataSource dataSource() {
+
+		return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
+	}
+}
