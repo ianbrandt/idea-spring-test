f418953b910bcdb753083ae2dd3122ba1df73b5b
Ian Brandt <ian@ianbrandt.com>
2015-06-01 09:52:28 -0700

    Switched from injecting a DataSource (interface) to a JdbcTemplate (concrete class) instead.  No change.

--- src/test/java/DbTest.java
+++ src/test/java/DbTest.java
@@ -5,8 +5,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import javax.sql.DataSource;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -16,14 +14,12 @@ public class DbTest {
 	public static final int EXPECTED = 1;
 
 	@Autowired
-	public DataSource dataSource;
+	public JdbcTemplate jdbcTemplate;
 
 	@Test
 	public void testDb() {
 
-		final JdbcTemplate template = new JdbcTemplate(dataSource);
-
-		final Integer queryResult = template.queryForObject("select " + EXPECTED, Integer.class);
+		final Integer queryResult = jdbcTemplate.queryForObject("select " + EXPECTED, Integer.class);
 
 		assertThat(queryResult).isEqualTo(EXPECTED);
 	}
--- src/test/java/DbTestConfig.java
+++ src/test/java/DbTestConfig.java
@@ -1,5 +1,6 @@
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
@@ -12,5 +13,11 @@ public class DbTestConfig {
 	public DataSource dataSource() {
 
 		return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
+	}
+
+	@Bean
+	public JdbcTemplate jdbcTemplate() {
+
+		return new JdbcTemplate(dataSource());
 	}
 }
